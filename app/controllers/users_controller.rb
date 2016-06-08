4cb9d2d90ca9eb208b895f091107b2d8ca283a18
enoch232 <enoch232@gmail.com>
2016-06-08 16:33:01 -0700

    add user controller with new, index, and create actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			render :new
+		else
+			redirect_to :index
+		end
+	end
+	def index
+		@users = User.all
+	end
+	private
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+end
